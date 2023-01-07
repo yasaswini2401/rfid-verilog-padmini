309892495a9971009d8bf9d4c6dfc91b3b1f5fd8
minipali <96111566+minipali@users.noreply.github.com>
2023-01-07 16:32:28 +0530

    Update top.v
    with preamble indicator and calibration control as well

--- tag/top.v
+++ tag/top.v
@@ -34,7 +34,11 @@ module top(reset, clk, demodin, modout, // regular IO
            ///transmit clock
            pll_enable, freq_channel, rforbase,
            ////from sample data
-           adc_sample, senscode);
+           adc_sample, senscode,
+           /////
+           morb_trans,
+           crc5invalid, crc16invalid, bitout,
+           preamble_indicator, calibration_control);
 
   // Regular IO
   // Oscillator input, master reset, demodulator input
@@ -64,13 +68,26 @@ module top(reset, clk, demodin, modout, // regular IO
   input  debug_clk;
   output debug_out;
   
-  ///
+  ///transmit clock command
   output pll_enable; /// final control for pll
   output [3:0] freq_channel;
   output rforbase;
+  
   ////
   output [2:0] senscode;
   output adc_sample;
+  /////
+  output morb_trans;
+  //crc5
+  output crc5invalid, crc16invalid;
+  
+  output wire preamble_indicator;
+  output wire calibration_control;
+  
+  reg ext_packetcomplete;
+  
+  
+
   
 
   // CONTROLLER module connections
@@ -81,12 +98,13 @@ module top(reset, clk, demodin, modout, // regular IO
   wire adc_sample;
 
   // RX module connections
-  wire rx_reset, rxtop_reset, bitclk, bitout;
+  wire rx_reset, rxtop_reset, bitclk;
+  output wire bitout;
   wire rx_overflow;
 
   // PACKET PARSE module connections
   wire       handlematch;
-  wire [1:0] readwritebank;
+  wire [1:0] readwritebank;//use for read, write, select
   wire [7:0] readwriteptr;
   wire [7:0] readwords;
   wire       writedataout, writedataclk;
@@ -98,12 +116,27 @@ module top(reset, clk, demodin, modout, // regular IO
   wire rforbase;
   ////
   wire [2:0] senscode;
+  /////
+  wire morb_trans;
+  //for select: extra wires to send to the memory module:
+  wire [2:0] sel_target;
+  wire [2:0] sel_action;
+  wire [1:0] sel_bank;
+  wire [7:0] sel_ptr;
+  wire [7:0] sel_masklen;
+  
+  
+  
+  
+  
   
   
 
   // CMDPARSE module connections
   wire packet_complete, cmd_complete;
   output wire [11:0] rx_cmd;
+      //crc5, crc16 checks
+      wire crc5invalid, crc16invalid;
 
   // TX module connections
   wire tx_reset, txsetupdone, tx_done;
@@ -130,7 +163,7 @@ module top(reset, clk, demodin, modout, // regular IO
   
   
   // RX and TX module reset signals
-  assign tx_reset    = reset | !tx_en;
+  assign tx_reset    = reset | !tx_en; // transmission is not allowed if crc5 is invalid
   assign rx_reset    = reset | !rx_en;
   assign rxtop_reset = reset | !rx_en;
   
@@ -145,6 +178,9 @@ module top(reset, clk, demodin, modout, // regular IO
   parameter BITSRC_EPC  = 1;
   parameter BITSRC_READ = 2;
   parameter BITSRC_UID  = 3;
+  
+//bitsrc all msb first
+
 
   // mux the bit source for the tx module
   wire [3:0] bitsrc;
@@ -193,10 +229,11 @@ module top(reset, clk, demodin, modout, // regular IO
     if(reset) begin
       debug_address <= 4'd0;
     end else begin
-      debug_address <= debug_address + 4'd1;
+      //debug_address <= debug_address + 4'd1;
+      debug_address <= 4'd0;
     end
   end
-  always @ (debug_address) begin
+  always @ (debug_clk) begin
   case(debug_address)
     0:  debug_out = packet_complete;
     1:  debug_out = cmd_complete;
@@ -217,6 +254,14 @@ module top(reset, clk, demodin, modout, // regular IO
     default: debug_out = 1'b0;
   endcase
   end
+  
+  assign calibration_control = ~(ext_packetcomplete);
+  
+  always@(posedge packet_complete or posedge preamble_indicator) begin
+    if(preamble_indicator == 0) begin
+        ext_packetcomplete <= 1;
+    end else ext_packetcomplete <= 0;
+  end
 
   // MODULES! :)
 
@@ -228,15 +273,16 @@ module top(reset, clk, demodin, modout, // regular IO
                     ///
                     plloff,
                     ////
-                    adc_sample);
+                    adc_sample, 
+                    crc5invalid, crc16invalid);
 
   txsettings U_SET (reset, trcal_in,  m_in,  dr_in,  trext_in, query_complete,
                            trcal_out, m_out, dr_out, trext_out);
 
-  rx        U_RX  (rx_reset, clk, demodin, bitout, bitclk, rx_overflow, trcal_in, rngbitin);
+  rx        U_RX  (rx_reset, clk, demodin, bitout, bitclk, rx_overflow, trcal_in, rngbitin, preamble_indicator);
   
-  cmdparser U_CMD (rxtop_reset, bitout, bitclk, rx_cmd, packet_complete, cmd_complete,
-                   m_in, trext_in, dr_in);
+  cmdparser U_CMD (rxtop_reset, clk, bitout, bitclk, rx_cmd, packet_complete, cmd_complete,
+                   m_in, trext_in, dr_in,crc5invalid,crc16invalid );
 
   packetparse U_PRSE (rx_reset, bitout, bitclk, rx_cmd, rx_q, rx_updn,
                       currenthandle, currentrn, handlematch,
@@ -245,11 +291,13 @@ module top(reset, clk, demodin, modout, // regular IO
                       ///
                       pllenab, freq_channel,rforbase,
                       ////
-                      senscode);
+                      senscode,
+                      /////,
+                      morb_trans);
 
   rng       U_RNG  (tx_reset, reset, rngbitin, rngbitinclk, rngbitclk, rngbitsrc, rngdatadone, currentrn);
   
-  epc       U_EPC  (tx_reset, epcbitclk, epcbitsrc, epcdatadone);
+  epc_linemem       U_EPC  (tx_reset, epcbitclk, epcbitsrc, epcdatadone);
   
   read      U_READ (tx_reset, readbitclk, readbitsrc, readdatadone, 
                     read_sample_ctl, read_sample_clk, read_sample_datain, 
@@ -257,9 +305,14 @@ module top(reset, clk, demodin, modout, // regular IO
                     
   uid       U_UID  (tx_reset, uidbitclk, uidbitsrc, uiddatadone, 
                     uid_byte_in, uid_addr_out, uid_clk_out);
-
-  sequencer U_SEQ (tx_reset, rx_overflow, clk, m_out, dr_out, docrc, trext_out, 
-                   trcal_out, txbitsrc, txdatadone, txbitclk, modout, txsetupdone, tx_done);
+                    
 //module sequencer (reset, rtcal_expired, oscclk, m, dr, docrc, trext, trcal, 
 //                  databitsrc, datadone, dataclk, modout, txsetupdone, txdone);
+  sequencer U_SEQ (tx_reset, rx_overflow, clk, m_out, dr_out, docrc, trext_out, 
+                   trcal_out, txbitsrc, txdatadone, txbitclk, modout, txsetupdone, tx_done);
+
+
+ //adding extra module epc, for memory purpose
+  //epc_memorymodule(clk, reset, curr_sl_flag, curr_inven_flag)
+                   
 endmodule
