4f23cfb44a3f693e96dcf17082d47e21187246ab
minipali <96111566+minipali@users.noreply.github.com>
2023-01-15 14:11:47 +0530

    Update and rename epc.v to epc_linemem.v

--- tag/epc_linemem.v
+++ tag/epc_linemem.v
@@ -8,7 +8,7 @@
 //  2 byte  PC: 0x30, 0x00
 // 12 byte EPC: 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55
 
-module epc(reset, epcclk, epcbitout, epcdone);
+module epc_linemem(reset, epcclk, epcbitout, epcdone);
 input reset, epcclk;
 output epcbitout, epcdone;
 
